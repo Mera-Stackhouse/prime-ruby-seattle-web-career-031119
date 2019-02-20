78e642fbef550ca66f3500a00864899fb622c1a8
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 07:47:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,14 @@
 # Add  code here!
 
 def prime?(num)
-  puts "Be sure to account for negative numbers!" unless num.is_a? Integer
-  for i in 2..(num - 1)
-    if num % i == 0
-      false
+  if num.is_a? Integer
+    puts "Not a valid input"
+  else
+    for i in 2..(num - 1)
+      if num % i == 0
+        false
+      end
     end
+    true
   end
-  true
 end

918420e009307d51c54e160c7563ba19fb40806f
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 07:45:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 
 def prime?(num)
-  puts "Please enter positive integer" unless num == integer
+  puts "Please enter positive integer" unless num.is_a? Integer
   for i in 2..(num - 1)
     if num % i == 0
       false
